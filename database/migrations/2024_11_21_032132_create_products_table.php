93f473fba66f2064d478078cd3885bc47986f916
ikyy2001 <vitogaming65@gmail.com>
2024-12-03 16:33:56 +0700

    Vidi NT

--- database/migrations/2024_11_21_032132_create_products_table.php
+++ database/migrations/2024_11_21_032132_create_products_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->text('about');
             $table->timestamps();
-        });
+        });  
     }
 
     /**
